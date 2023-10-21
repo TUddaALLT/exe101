b2ccb8edd7692a4aed9bab4e515b3d076f20167a
tudda <tentufancr7@gmail.com>
2023-10-21 14:09:18 +0700

    .

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const jsonServer = require("json-server"); // importing json-server library
+const express = require("express");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const port = process.env.PORT || 3001;
+const app = express();
 
 server.use(middlewares);
 server.use(router);
 
-// Define a route to handle updating "numberOfStar" by email using a GET request
-server.get("/updateNumberOfStar", (req, res) => {
-  const phoneno = req.query.phone; // Get the email from the query parameter
+// Define a route to handle updating "numberOfStar" by email
+app.post("/updateNumberOfStar", (req, res) => {
+  const emailToUpdate = req.query.phone; // Get the email from the query parameter
+  const updatedNumberOfStar = Number(req.query.updatedNumberOfStar); // Get the updated number of stars
 
   // Find the user with the specified email
-  const user = router.db.get("users").find({ phone: phoneno }).value();
+  const user = router.db.get("users").find({ phone: emailToUpdate }).value();
 
   if (!user) {
     return res.status(404).json({ error: "User not found" });
   }
 
-  // Increment the "numberOfStar" property
-  user.numberOfStar = (parseInt(user.numberOfStar) || 0) + 1;
+  // Update the "numberOfStar" property
+  user.numberOfStar = updatedNumberOfStar;
 
   // Save the updated data to the database
   router.db.write();
